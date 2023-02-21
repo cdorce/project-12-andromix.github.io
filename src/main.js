f1613f2d360a9f94590c68e9c46073391e873b1e
Cleffton Dorce <67771337+cdorce@users.noreply.github.com>
2023-02-20 19:26:41 -0800

    section-2-done

--- src/main.js
+++ src/main.js
@@ -22,4 +22,36 @@ hamburger.addEventListener("click", () => {
 
 
 
-  
+  const texts = document.querySelector(".animate-text").children,
+        textsLen = texts.length;
+        let index = 0;
+        const textInTimer=3000,
+              textOutTimer=2800;
+
+  function animateText() {
+
+    for(let i=0; i<textsLen; i++) {
+      texts[i].classList.remove("text-in", "text-out");
+
+    }
+
+    texts[index].classList.add("text-in");
+
+    setTimeout(function () { 
+      texts[index].classList.add("text-out");
+
+     }, textOutTimer)
+
+
+    if(index == textsLen-1) {
+      index=0;
+    } else {
+      index++;
+    }
+
+
+    setTimeout(animateText, 3000);
+
+  }
+
+  window.onload=animateText;
